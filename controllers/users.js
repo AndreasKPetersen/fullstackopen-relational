f53c28d8d032d30de11623343f4250a0235d3dcd
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-09-26 08:50:58 +0200

    Task 13.8.

---
+++ controllers/users.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+
+const { User } = require("../models");
+
+router.get("/", async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+});
+
+router.get("/:username", async (req, res) => {
+  const user = await User.findOne({ where: { username: req.params.username } });
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.put("/:username", async (req, res) => {
+  const userNewUsername = await User.findOne({
+    where: { username: req.body.username },
+  });
+  if (!req.body.username) {
+    return res.status(400).end();
+  } else if (userNewUsername) {
+    return res.status(400).end();
+  }
+
+  const user = await User.findOne({ where: { username: req.params.username } });
+  if (user) {
+    user.username = req.body.username;
+    await user.save();
+    res.json(user);
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -6,11 +6,16 @@ require("express-async-errors");
 const app = express();
 
 const blogsRouter = require("./controllers/blogs");
+const loginRouter = require("./controllers/login");
+const usersRouter = require("./controllers/users");
+
 const errorHandler = require("./util/errorHandler");
 
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(errorHandler);
 
