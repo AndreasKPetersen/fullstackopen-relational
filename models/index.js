d8e89bc65dfb054c6a2af51a6bfc61b966ac7456
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-09-29 20:30:25 +0200

    Task 13.19.

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Blog = require("./blog");
+const Reading = require("./reading");
 const User = require("./user");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
+User.belongsToMany(Blog, { through: Reading });
+Blog.belongsToMany(User, { through: Reading });
+
 module.exports = {
   Blog,
+  Reading,
   User,
 };
