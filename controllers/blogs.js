b02ce990e340d679383befc60f6b5c312c3592c6
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-10-05 17:47:32 +0200

    Exercise 13.22.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,24 +1,9 @@
-const jwt = require("jsonwebtoken");
 const { Op } = require("sequelize");
 const router = require("express").Router();
 
-const { SECRET } = require("../util/config");
-
 const { Blog, User } = require("../models");
 
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
+const tokenExtractor = require("../util/tokenExtractor");
 
 router.get("/", async (req, res) => {
   const where = {};
